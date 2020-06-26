55fc8828e8972d3c011b7650123784ef36930e58
Yainel García Alfonso <garcia.yainel@gmail.com>
2020-06-26 10:31:00 -0400

    Incluida la API al formulario de reservas en hostal.html

---
+++ assets/js/lightgalery.js
@@ -0,0 +1,9 @@
+$('.lightgallery').lightGallery({
+    share: false,
+    thumbnail: false,
+    download: false,
+    autoplayControls: false,
+    zoom: false,
+    fullScreen: false,
+    selector: '.house-slider'
+});
